ba25560d3caaf5e74f6d7400fea58b0416408ff1
Iamthenano <119520890+Iamthenano@users.noreply.github.com>
2022-12-08 16:25:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,34 @@
 using namespace std;
 
 int main() {
-    cout << "penis!";
+ 
+}    
 
-    return 0;
-}
+int getNumSeatsinRow(int column,int row, char seatChart[][])
+{
+    
+    int numAvailSeats[];
+    int count = 0;
+    int total = 0;
+    for( int i = 0; int < row; i++)
+    {
+        for(int j = 0; j < 15; j++)
+        {
+            if(seatchart[i][j] = "#')
+              {
+                    count++;  
+                    cout << seatchart[i][j] << " is available ";
+              } 
+            else
+               {
+                   cout << seatchart[i][j] << " is taken ";
+               }    
+        }       
+        
+    total += count;
+    numAvailSeats[i] = count;
+    
+    count = 0;        
+    }
+return total;              
+}    
