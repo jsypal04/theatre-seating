4b03f96b2b21f8b126cc0aababd75892c1b97322
John Paul Abela <59853197+JPAbela@users.noreply.github.com>
2022-12-08 15:01:26 -0500

    add printseatchart file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-int main() {
-    cout << "hello world!";
+void printSeatChart(char seatChart[][30])
+{
+    cout <<setw(26) <<"Seats" <<endl;
+    cout <<setw(40) <<right <<"123456789012345678901234567890" <<endl;
+    for(int row = 0; row < 15; row ++)
+    {
+        cout <<"Row " <<row;
+        if(row < 10)
+            cout <<"     ";
+        else
+            cout <<"    ";
+        
+        for(int seat = 0; seat < 30; seat++)
+        {
+            cout <<seatChart[row][seat];
+        }
+        cout <<endl;
+    }
 
+}
+
+int main() {
+    
     return 0;
 }
